8d6f5adf9c5d75b33a34e6d433b1460ab3713154
SynAcktraa <harshmetaexploit90@gmail.com>
2022-03-18 01:20:21 +0530

    idk

--- toString_utils.h
+++ toString_utils.h
@@ -6,7 +6,7 @@ int octToDec(int oct);
 int hexToDec(char* hex);
 int binToDec(char* bin);
 int power(int base, int exp);
-int AtoI(const char* str);
 int isDigit(int ch);
+int AtoI(const char* str);
 
 #endif

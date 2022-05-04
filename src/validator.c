47bc80f54ed658e644f27d2158309a2989cce86b
SynAcktraa <harshmetaexploit90@gmail.com>
2022-05-05 00:43:10 +0530

    enhanced validating functions

--- src/validator.c
+++ src/validator.c
@@ -1,13 +1,15 @@
 #include <string.h>
 #include "utils.h"
+#include<stdio.h>
 
 int validateOctalValue(char*octal_value) {
 
     /* Octal values can't have the value 8 and 9,
        so searches for them, if present
        it returns -1 */
-    if(strcspn(octal_value, "89") != Strlen(octal_value))
+    if((int)strcspn(octal_value, "89") != Strlen(octal_value)){
         return -1;
+    }
 
     return 0;
 }
@@ -16,7 +18,8 @@ int validateHexValue(char*hex_value) {
 
     /* Hex values can't have values other than
        "123456789ABCDEF" */
-    if(strspn(hex_value, "0123456789ABCDEFabcdef ") != Strlen(hex_value)){
+    if((int)strspn(hex_value, "0123456789ABCDEFabcdef \n") != Strlen(hex_value)){
+        puts(hex_value);
         return -1;
     }
 
@@ -26,10 +29,12 @@ int validateHexValue(char*hex_value) {
 int validateIntValue(char*int_value) {
 
     for (int i = 0; int_value[i] != '\0'; i++){
-        if(int_value[i] == ' ')
+        if(int_value[i] == ' ' || int_value[i] == '\n')
             continue;
-        if (!isDigit(int_value[i]))
+
+        if (!isDigit(int_value[i])){
             return -1;
+        }
     }
 
     return 0;
@@ -40,22 +45,14 @@ int validateBinValue(char*bin_value) {
     /* Binary values can't have values other 
        than 0 and 1 */
     for(int i = 0; i < Strlen(bin_value); ++i){
-        if(bin_value[i] == ' ')
-            continue;
-        if(*(bin_value+i) != 48 && *(bin_value+i) != 49)
+
+        if(*(bin_value+i) != 48 && *(bin_value+i) != 49){
             return -1;
+        }
     }
 
-    if(binToDec(bin_value) > 126)
-        return -2;
+    if(binToDec(bin_value) > 126){
+        return -2;}
 
-    return 0;
-}
-
-
-int charValidator(char ch) {
-
-    if(ch < 0 || ch > 126)
-        return -1;
     return 0;
 }

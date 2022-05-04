183741165f7f5ff67ec4273ccaa67670983fb2e8
SynAcktraa <harshmetaexploit90@gmail.com>
2022-05-05 01:07:40 +0530

    optimized file reading speed

--- src/file_handler.c
+++ src/file_handler.c
@@ -18,49 +18,36 @@ int checkIfFileExists(const char * file) {
 
 char* get_file_data(const char*file) {
 
-/*
-    Checks if file exists on the system, if yes
-    stores the file size in buffer_len var
-    by evaulating get_filesize function which 
-    retrieves the file size and then reads the
-    file line by line and stores it in buffer 
-    and then concatenate it to data_storage
-    and finally frees the buffer and return data_storage
-*/
+
     if(!checkIfFileExists(file)) {
         fprintf(stderr, "FileError: can't open %s file.\n", file);
         exit(1);
     }
 
-    int buffsize = 1024;
+    int buffsize = 512;
     FILE * fptr = fopen(file, "rb");
     char* buffer = (char*)malloc(buffsize);
     
     if(buffer == NULL) exit(EXIT_FAILURE);
 
     int ch, cursor = 0;
-    do { 
-        do { 
-            ch = fgetc(fptr); //storing char in ch
-            
-            // if ch is not the end of file, buffer is appended with ch char value
-            if(ch != EOF){
-                if(isprint(ch) || ch == '\t')
-                    buffer[cursor++] = (char)ch;
-            } 
 
-            /* if cursor crosses current buffer size
-                it's doubled in size and new size gets reallocated */
-            if(cursor >= buffsize - 1) { 
-                buffsize <<=1;
-                buffer = (char*)realloc(buffer, buffsize);
-            }
-        } while(ch != EOF && ch != '\n'); // will continue until ch is not EOF and newline character
+    while(ch != EOF) { 
+        ch = fgetc(fptr); //storing char in ch
         
-        buffer[cursor++] ='\n';
-    } while(ch != EOF); // while ch is not end of the file
+        // if ch is not the end of file, buffer is appended with ch char value
+        if(isprint(ch) || ch == '\t' || ch == '\n')
+            buffer[cursor++] = (char)ch;
+
+        /* if cursor crosses current buffer size
+            it's doubled in size and new size gets reallocated */
+        if(cursor >= buffsize - 1) { 
+            buffsize <<=1;
+            buffer = (char*)realloc(buffer, buffsize);
+        }
+        
+    } // while ch is not end of the file
     
-    buffer[--cursor] = '\0';
     fclose(fptr); //always close the file pointer BAKA ^_^         
 
     return buffer;
